a89b81b6b411c70794ee5c0277eecb9244531f2f
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2025-04-26 19:55:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
     fs.createReadStream(path.join(__dirname, "public", "index.html")).pipe(res);
   } else {
     res.writeHead(500);
-    res.end("not foubd");
+    res.end("not found");
   }
 });
 
@@ -21,9 +21,6 @@ createTail(logFilePath, (line) => {
   io.emit("logLine", line);
 });
 io.on("connection", (socket) => {
-//   const allLines = fs.readFileSync(logFilePath, "utf8").split("\n");
-//   console.log("— file has", allLines.length, "total lines");
-//   console.log("conn", socket.id);
   const lastLines = getLastNLines(logFilePath, 10);
   socket.emit("initLog", lastLines);
   socket.on("disconnect", () => {
@@ -32,5 +29,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log("conn");
+  console.log("connected to PORT");
 });
